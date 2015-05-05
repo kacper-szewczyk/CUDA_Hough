ccfafd0716ca8b86abbdc345aa1fcd6acc4e0502
kszewcz2 <kszewcz2@bestia.(none)>
2015-05-05 09:31:05 +0200

    Load image

---
+++ file.h
@@ -0,0 +1,17 @@
+/*
+ * file.h
+ *
+ *  Created on: 28 kwi 2015
+ *      Author: kszewcz2
+ */
+
+#ifndef FILE_H_
+#define FILE_H_
+#include <stdio.h>
+
+
+int* read_from_file(int* image, char* filename, int *n, int *m);
+
+
+
+#endif /* FILE_H_ */
---
+++ main.c
@@ -0,0 +1,11 @@
+#include "file.h"
+
+int main()
+{
+	int *image;
+	int n;
+	int m;
+	image=read_from_file(image,"image.pgm",&n,&m);
+
+	return 0;
+}
