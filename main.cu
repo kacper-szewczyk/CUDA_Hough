ab57bb662d5175bab98dd1c90fccd0b7f8da6e4f
kszewcz2 <kszewcz2@bestia.(none)>
2015-06-16 17:35:55 +0200

    Final

--- main.cu
+++ main.cu
@@ -4,7 +4,6 @@
 
 int main(int argc, char ** argv)
 {
-<<<<<<< HEAD
 	char inputFile[] = { "mountain.pgm" };
 	char resultFile[] = { "Houghed.pgm" };
 	Image *image=readImageFromFile(inputFile);
@@ -13,7 +12,7 @@ int main(int argc, char ** argv)
 	int blocks = image->getWidth();
 	int threads = image->getHeight();
 	int size=image->getHeight()*image->getWidth();
-	size_t imageSize = (size+3) * sizeof(int);
+	//size_t imageSize = (size+3) * sizeof(int);
 	cudaMalloc((void**)&devImage,size*sizeof(int) );
 	cudaMemcpy(devImage,image->getArray(),size*sizeof(int),cudaMemcpyHostToDevice);
 	cudaMalloc((void**)&devTresholded,size*sizeof(int) );
@@ -51,109 +50,7 @@ int main(int argc, char ** argv)
 		//printf("%d ",result[i]);
 	}
 	image->setArray(result);
-	//saveImageToFile(result,resultFile);
-	/*
-	int n=1000;
-=======
-
-	Image *image=readImageFromFile("mountain.pgm");
-	for(int i=0;i<10;i++)
-	{
-		for(int j=0;j<10;j++)
-		{
-			image->getArray()[i];
-		}
-	}
-	/*int n=1000;
->>>>>>> ec5918de9505894795af882336c67e797ff38e0d
-	char inputFile[] = { "mountain.pgm" };
-	char outputFile[] = { "treshold.pgm" };
-	char resultFile[] = { "Houghed.pgm" };
-	Image *image=readImageFromFile(inputFile);
-<<<<<<< HEAD
-	/*char testFile[] = { "test.pgm" };
-=======
-	char testFile[] = { "test.pgm" };
->>>>>>> ec5918de9505894795af882336c67e797ff38e0d
-	saveImageToFileTest(testFile);
-	Image *devImage;
-	Image *devThresholdImage;
-	int blocks = image->getWidth();
-	int threads = image->getHeight(); // height of the image is max 480
-	Image *result = new Image(image->getWidth(),image->getHeight(),image->getScale());
-	/*
-	double *ro;
-	double *theta;
-	size_t steps = n*sizeof(int);
-	int *A;
-	int size=image->getHeight()*image->getWidth();
-	size_t size2 = size*sizeof(int);
-	int *indexes;
-	int *indexesHost = new int[size];
-	int *deviceImage;
-	cudaMalloc((void**)&devImage,sizeof(Image) );
-	cudaMalloc((void**)&deviceImage,size2 );
-	cudaMalloc((void**)&indexes,size2);
-	cudaMalloc((void**)&devThresholdImage,sizeof(Image) );
-	cudaMalloc((void**)&A,size2 );
-	cudaMalloc((void**)&ro,sizeof(double)* steps);
-	cudaMalloc((void**)&theta,sizeof(double)* steps);
-	cudaMemcpy(devImage,image,sizeof(Image),cudaMemcpyHostToDevice);
-	cudaMemcpy(devThresholdImage,result,sizeof(Image),cudaMemcpyHostToDevice);
-	printf("End of allocation\n");
-<<<<<<< HEAD
-	//makeLaplaceMask<<<blocks,threads>>>(devImage,
-	// devThresholdImage, size);
-	thresholdImage<<<blocks,threads>>>(deviceImage,devThresholdImage->getScale()/2,size);
-	int *imageTres = new int[size];
-	cudaMemcpy(imageTres,devThresholdImage,sizeof(Image), cudaMemcpyDeviceToHost);
-=======
-	makeLaplaceMask<<<blocks,threads>>>(devImage,devThresholdImage, size);
-	thresholdImage<<<blocks,threads>>>(devThresholdImage,devThresholdImage->getScale()/2,size);
-	cudaMemcpy(result,devThresholdImage,sizeof(Image), cudaMemcpyDeviceToHost);
-	cudaMemcpy(B,deviceImage,size2, cudaMemcpyDeviceToHost);
->>>>>>> ec5918de9505894795af882336c67e797ff38e0d
-	for(int i=0;i<size;i++)
-	{
-		printf("%d ",imageTres[i]);
-	}
-	/*cudaMemcpy(B,deviceImage,size2, cudaMemcpyDeviceToHost);
-	for(int i=0;i<size;i++)
-	{
-		printf("%i ",B[i]);
-	}houghTransform<<<blocks,threads>>>(devThresholdImage,
-	 ro, theta, A, steps, steps);
-	printf("After treshold\n");
-    saveImageToFile(result,outputFile);
-	double roMax = sqrt(image->getHeight()*image->getHeight() + 
-	image->getWidth()*image->getWidth());
-	createRoAndThetaArrays<<<blocks,threads>>>(ro, theta, roMax/steps, 3.14/steps, steps);
-	printf("After Ro theta arrays\n");
-	houghTransform<<<blocks,threads>>>(devThresholdImage,
-	 ro, theta, A, steps, steps);
-	printf("Hough\n");
-	findLocalMaximas<<<blocks,threads>>>(A, 5, indexes, size);
-	printf("Local Maximas\n");
-	//cudaMemcpy(result,devImage,sizeof(Image), cudaMemcpyDeviceToHost);
-	cudaMemcpy(indexesHost,indexes,sizeof(int)
-		*image->getHeight()*image->getWidth(),
-		cudaMemcpyDeviceToHost);
-	result->setArray(indexesHost);
-	printf("After set array\n");
-	saveImageToFile(result,resultFile);
-	cudaFree(theta);
-	cudaFree(ro);
-	cudaFree(A);
-	cudaFree(devImage);
-	cudaFree(deviceImage);
-	cudaFree(indexes);
-	cudaFree(devThresholdImage);
-	free(image);
-	free(result);
-<<<<<<< HEAD
-	//free(B);*/
-=======
-	free(B);*/
->>>>>>> ec5918de9505894795af882336c67e797ff38e0d
+	saveImageToFile(image,resultFile);
+	
 	return 0;
 }
